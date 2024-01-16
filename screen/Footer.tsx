ea0c06a24a5bfc2537a00581e4d768ef84cfe98e
mindfree4u <mindfree4u@daum.net>
2024-01-16 20:52:29 +0900

    footer 크지 조정

--- screen/Footer.tsx
+++ screen/Footer.tsx
@@ -2,6 +2,9 @@ import React, { useCallback, useState } from 'react';
 import { View, Image, Text, StyleSheet,TouchableOpacity, StatusBar} from 'react-native';
 import type {StatusBarStyle} from 'react-native';
 import Home_btn from '../images/SVG/home.svg';
+import { SvgUri } from 'react-native-svg';
+
+
 
 
 const STYLES = ['default', 'dark-content', 'light-content'] as const;
@@ -17,6 +20,16 @@ const Footer = ({ navigation, selectedIconIndex, setSelectedIconIndex}) => {
   const [statusBarTransition, setStatusBarTransition] = useState<
     'fade' | 'slide' | 'none'
   >(TRANSITIONS[0]);
+
+const Home_Btn: React.FC = () => {
+	return (
+		<SvgUri 
+			width="44"
+			height="44"
+			source={require('../images/SVG/home_right.svg')}
+		/>
+	);
+};
 
 
  return (
@@ -34,23 +47,22 @@ const Footer = ({ navigation, selectedIconIndex, setSelectedIconIndex}) => {
   <View style={styles.line } />
 	<View style={styles.container}>
 		<TouchableOpacity onPress={() => {navigation.navigate('Page_10000'); setSelectedIconIndex(0); }} style={{width: 50, marginLeft: 20}}>
-			
 			{ selectedIconIndex === 0 ? <Image source={require('../images/short_line.png')} style={[styles.short_line, {left:34}]}/> : <Text style={{height: 12}}></Text>}
-			{ selectedIconIndex === 0 ? <Image source={require('../images/home.png')} style={{width: 30, height: 30, left: 45}}/> : <Image source={require('../images/home.png')} style={{width: 30, height: 30, left: 45, opacity:0.5}}/>  }
+			{ selectedIconIndex === 0 ? <Image source={require('../images/home.png')} style={{left: 49}}/> : <Image source={require('../images/home.png')} style={{left: 49, opacity:0.5}}/>  }
 			{ selectedIconIndex === 0 ? <Text style={[styles.text1, {left: 53}, selectedIconIndex === 0 ? {color:'#000000'} : {color:'#9190B2'}]}>홈</Text> : <Text style={[styles.text1, {left: 53, fontWeight: 400}, selectedIconIndex === 0 ? {color:'#000000'} : {color:'#9190B2'}]}>홈</Text>}
 		</TouchableOpacity>
 		
 		
 		 <TouchableOpacity onPress={() => {navigation.navigate('Page_50000'); setSelectedIconIndex(4); }} style={{width: 50}}>
 			{ selectedIconIndex === 4 ? <Image source={require('../images/short_line.png')} style={[styles.short_line, {left:9}]}/> : <Text style={{height: 12}}></Text>}
-			{ selectedIconIndex === 4 ? <Image source={require('../images/message.png')} style={{width: 30, height: 30, left:21 }}/> : <Image source={require('../images/message.png')} style={{width: 30, height: 30, left:21, opacity: 0.5 }}/> }
+			{ selectedIconIndex === 4 ? <Image source={require('../images/message.png')} style={{left:24 }}/> : <Image source={require('../images/message.png')} style={{left:24, opacity: 0.5 }}/> }
 			{ selectedIconIndex === 4 ? <Text style={[styles.text1,{left: 22}, selectedIconIndex === 4 ? {color:'#000000'} : {color:'#9190B2'}]}>알림</Text> : <Text style={[styles.text1,{left: 22, fontWeight:400}, selectedIconIndex === 4 ? {color:'#000000'} : {color:'#9190B2'}]}>알림</Text> }
 		</TouchableOpacity>
 		
 		
 		<TouchableOpacity onPress={() => {navigation.navigate('Page_60000'); setSelectedIconIndex(5); }} style={{width: 50, marginRight: 20}}>
 			{ selectedIconIndex === 5 ?  <Image source={require('../images/short_line.png')} style={[styles.short_line, {left: -10}]}/> : <Text style={{height: 12}}></Text>}
-			{ selectedIconIndex === 5 ?  <Image source={require('../images/my.png')} style={{width: 30, height: 30, left: 1}}/> : <Image source={require('../images/my.png')} style={{width: 30, height: 30, left:1, opacity: 0.5}}/>}
+			{ selectedIconIndex === 5 ?  <Image source={require('../images/my.png')} style={{left: 3}}/> : <Image source={require('../images/my.png')} style={{left:3, opacity: 0.5}}/>}
 			{ selectedIconIndex === 5 ?  <Text style={[styles.text1, {left: -17, width: 100},  selectedIconIndex === 5 ? {color:'#000000'} : {color:'#9190B2'}]}>마이페이지</Text> : <Text style={[styles.text1, {left: -17, width: 100, fontWeight: 400},  selectedIconIndex === 5 ? {color:'#000000'} : {color:'#9190B2'}]}>마이페이지</Text> }
 		</TouchableOpacity>
 
